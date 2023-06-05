e1fdea03b2e53991b9cc8caf911ad52629c95286
蹊兮 <tingyan.xty@alibaba-inc.com>
2023-06-05 17:41:46 +0800

    update repo

--- model/layers.py
+++ model/layers.py
@@ -1,10 +1,7 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 import numpy as np
 import math
-from utils.utils import freeze_net
 
 
 def gelu(x):
